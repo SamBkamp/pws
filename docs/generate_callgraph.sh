367c7da0ea1e186c29c5776336343c77db32b8df
Sam Bonnekamp <sam@bonnekamp.net>
2025-12-03 19:33:18 +0800

    fancy new callgraph

--- docs/generate_callgraph.sh
+++ docs/generate_callgraph.sh
@@ -2,5 +2,5 @@
 
 rm pws
 make CFLAGS=-fdump-rtl-expand
-egypt *.expand | dot -Tpng -Gratio=0.5 -Nfontcolor=white -Ecolor=white -Ncolor=white -Gbgcolor=black  -o resources/callgraph.png
+egypt *.expand | dot -Tpng -Gratio=0.5 -Nfontcolor=white -Ecolor=white -Ncolor=white -Gbgcolor=black  -o docs/callgraph.png
 rm *.expand

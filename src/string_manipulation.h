81cb36854ee8644f1f24817f37a04dd99df0f9a5
Sam Bonnekamp <sam@bonnekamp.net>
2025-12-09 00:44:48 +0800

    forgot to commit file sigantures lol

--- src/string_manipulation.h
+++ src/string_manipulation.h
@@ -1,11 +1,13 @@
 //to take care of string manipulation code, file I/O requests parsing that kinda thing
 #ifndef PWS_STRING_MANIPULATION
 #define PWS_STRING_MANIPULATION
-char *format_dirs(char *path, char *ret_path, char *document_root);
+#include <poll.h>
+#include <stdint.h>
+char *format_dirs(char *path, char *ret_path, size_t ret_path_size,char *document_root);
 int parse_first_line(http_request *req, char* first_line);
 int parse_http_request(http_request *req, char* data);
 void free_http_request(http_request *req);
-char* long_to_ip(char* out, unsigned long IP);
+char* long_to_ipstr(unsigned long IP);
 char *get_file_type(char* path);
 void print_SSL_errstr(int SSL_err, FILE* stream);
 char *open_file(char *path, long *bytes);

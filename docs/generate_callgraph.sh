48f9c33f198871cbbc44a5cd050af7d5ced12bc6
Sam Bonnekamp <sam@bonnekamp.net>
2025-12-11 16:49:18 +0800

    updated callgraph and callgraph generator

--- docs/generate_callgraph.sh
+++ docs/generate_callgraph.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm pws
-make CFLAGS=-fdump-rtl-expand
+make dev CFLAGS=-fdump-rtl-expand
 egypt *.expand | dot -Tpng -Gratio=0.5 -Nfontcolor=white -Ecolor=white -Ncolor=white -Gbgcolor=black  -o docs/callgraph.png
 rm *.expand
